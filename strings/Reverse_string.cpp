b566bd68c6caddd55b00d1f306e3c21402dd7687
mayanksurana81 <mayanksurana81@gmail.com>
2026-03-10 21:02:57 +0530

    solve Leetcode[344] : Reverse a string using 2 pointer method

---
+++ strings/Reverse_string.cpp
@@ -0,0 +1,8 @@
+//leetcode 344 Reverse a String
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0 , j = s.size()-1;
+        while(i<=j) swap(s[i++] , s[j--]);
+    }
+};
