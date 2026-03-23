6dceefb1cdec57a23400b5bb44dcbf7322ac5f23
mayanksurana81 <mayanksurana81@gmail.com>
2026-03-23 19:24:36 +0530

    solve Leetcode[35] : Search insert position using binary search

---
+++ Linked List/Search_Insert_Position.cpp
@@ -0,0 +1,28 @@
+/*
+Category: Array (Primary), Binary Search (Secondary)
+
+Approach:
+Apply binary search to find the target. If found, return index.
+Otherwise, return the position where it would be inserted
+to maintain sorted order (i pointer).
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+
+Where n = number of elements.
+*/
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int i = 0;
+        int j = n-1;
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) i = mid+1;
+            else j = mid-1; 
+        }
+        return i;
+    }
+};
