27b41467a8ab6a516f799e2a62c050bc34785ea5
mayanksurana81 <mayanksurana81@gmail.com>
2026-06-19 17:42:05 +0530

    sort the singly linked list using Insertion Sort

--- Arrays/rough.cpp
+++ Arrays/rough.cpp
@@ -1,26 +1,26 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void selection_sort(vector<int>& arr){
+void insertion_sort(vector<int>& arr){
     int n = arr.size();
-    for(int i = 0; i < n-1; i++){
-        int mini = i;
-        for(int j = i; j <= n -1; j++){
-            if(arr[j] < arr[mini]){
-                mini = j;
-            }
+    for(int i = 0; i < n; i++){
+        int j = i;
+        while(j > 0 && arr[j] < arr[j - 1]){
+            int temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
         }
-        int temp = arr[i];
-        arr[i] = arr[mini];
-        arr[mini] = temp;
     }
+    
 }
 int main(){
-    vector<int> arr = {5,2,3,1};
+    vector<int> arr = {5,2,3,1,-9,58,-89,-45,20,10};
     for(int i = 0; i < arr.size(); i++){
         cout << arr[i] << " ";
     }
-    selection_sort(arr);
+    cout<<endl;
+    insertion_sort(arr);
     for(int i = 0; i < arr.size(); i++){
         cout << arr[i] << " ";
     }
